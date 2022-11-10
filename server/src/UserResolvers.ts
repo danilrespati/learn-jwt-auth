2a6f9f85fa998914b2e94e8cf55bda9c08b6910f
[Taufiq Dhanial] <[taufiq.dhanil@gmail.com]>
2022-11-10 16:59:36 +0700

    initial commit

---
+++ server/src/UserResolvers.ts
@@ -0,0 +1,84 @@
+import {
+  Resolver,
+  Query,
+  Mutation,
+  Arg,
+  ObjectType,
+  Field,
+  Ctx,
+  UseMiddleware,
+} from "type-graphql";
+import { User } from "./entity/User";
+import { hash, compare } from "bcryptjs";
+import { MyContext } from "./MyContext";
+import { createAccessToken, createRefreshToken } from "./auth";
+import { isAuth } from "./isAuth";
+
+@ObjectType()
+class LoginResponse {
+  @Field()
+  accessToken: string;
+}
+
+@Resolver()
+export class UserResolvers {
+  @Query(() => String)
+  hello() {
+    return "hi!";
+  }
+
+  @Query(() => String)
+  @UseMiddleware(isAuth)
+  bye(@Ctx() { payload }: MyContext) {
+    return `your user id is ${payload!.userId}`;
+  }
+
+  @Query(() => [User])
+  users() {
+    return User.find();
+  }
+
+  @Mutation(() => LoginResponse)
+  async login(
+    @Arg("email") email: string,
+    @Arg("password") password: string,
+    @Ctx() { res }: MyContext
+  ): Promise<LoginResponse> {
+    const user = await User.findOne({ where: { email } });
+    if (!user) {
+      throw new Error("could not find user");
+    }
+
+    const valid = await compare(password, user.password);
+    if (!valid) {
+      throw new Error("bad password");
+    }
+
+    res.cookie("jid", createRefreshToken(user), {
+      httpOnly: true,
+    });
+
+    return {
+      accessToken: createAccessToken(user),
+    };
+  }
+
+  @Mutation(() => Boolean)
+  async register(
+    @Arg("email") email: string,
+    @Arg("password") password: string
+  ) {
+    const hashedPassword = await hash(password, 12);
+
+    try {
+      await User.insert({
+        email,
+        password: hashedPassword,
+      });
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+    return true;
+  }
+}
